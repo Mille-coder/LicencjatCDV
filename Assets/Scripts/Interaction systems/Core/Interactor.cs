677b7b8497fd64dcbfe3a60da40d155cbc289a41
Plawinska <mplawinska@edu.cdv.pl>
2026-03-26 23:09:27 +0100

    DZIAŁA

--- Assets/Scripts/Interaction systems/Core/Interactor.cs
+++ Assets/Scripts/Interaction systems/Core/Interactor.cs
@@ -7,32 +7,40 @@ public class Interactor : MonoBehaviour
     [SerializeField] private float castDistance = 5f;
     [SerializeField] private Vector3 raycastOffset = new Vector3(0, 1f, 0);
 
+    private Renderer[] currentRenderers;
+    private IInteractable currentInteractable;
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (InteractionTest(out IInteractable interactable))
         {
-            if(InteractionTest(out IInteractable interactable))
+            Highlight(interactable);
+
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 Debug.Log("Interacted");
                 interactable.Interact(this);
             }
         }
+        else
+        {
+            ClearHighlight();
+        }
     }
+
     private bool InteractionTest(out IInteractable interactable)
     {
         interactable = null;
+
         Ray ray = new Ray(transform.position + raycastOffset, transform.forward);
-        if(Physics.Raycast(ray, out RaycastHit hitInfo, castDistance))
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, castDistance))
         {
             interactable = hitInfo.collider.GetComponent<IInteractable>();
 
-            if( interactable != null)
+            if (interactable != null)
             {
-                if(interactable.CanInteract(this))
-                {
-                    return true;
-                }
-                
+                return interactable.CanInteract(this);
             }
 
             return false;
@@ -41,4 +49,35 @@ public class Interactor : MonoBehaviour
         return false;
     }
 
+    private void Highlight(IInteractable interactable)
+    {
+        if (interactable == currentInteractable) return;
+
+        ClearHighlight();
+
+        currentInteractable = interactable;
+
+        GameObject obj = ((MonoBehaviour)interactable).gameObject;
+        currentRenderers = obj.GetComponentsInChildren<Renderer>();
+
+        foreach (var r in currentRenderers)
+        {
+            if (r.gameObject != obj)
+                r.material.SetFloat("_CanPickup", 1f);
+        }
+    }
+
+    private void ClearHighlight()
+    {
+        if (currentRenderers == null) return;
+
+        foreach (var r in currentRenderers)
+        {
+            if (r != null)
+                r.material.SetFloat("_CanPickup", 0f);
+        }
+
+        currentRenderers = null;
+        currentInteractable = null;
+    }
 }
