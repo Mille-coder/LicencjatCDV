afa7bf69dc7b2bee6557b02a6379ecd87bc31e78
Plawinska <mplawinska@edu.cdv.pl>
2026-05-03 15:26:00 +0200

    pauza poprzez esc

--- Assets/Scripts/UI/PauseMenu/PauseManager.cs
+++ Assets/Scripts/UI/PauseMenu/PauseManager.cs
@@ -16,7 +16,7 @@ public class PauseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             pauseCanvas.SetActive(true);
             Time.timeScale = 0;
