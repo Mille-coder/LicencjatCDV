00dd4cd75781bd057ce8ad229b73d71e8933f751
Plawinska <mplawinska@edu.cdv.pl>
2026-06-06 20:10:11 +0200

    poprawienie ui w tutorialu, dodanie efektu pojawiającego się tekstu w endingu

--- Assets/UI/Endscene.cs
+++ Assets/UI/Endscene.cs
@@ -1,22 +1,55 @@
+using TMPro;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using TMPro;
+using System.Collections;
 
 public class Endscene : MonoBehaviour
 {
     public GameObject mainMenu;      // Panel lub przycisk menu
     public VideoPlayer videoPlayer;  // Komponent VideoPlayer
 
+    [Header("Typing Text")]
+    public GameObject textPanel;     // Panel z tekstem ze sceny
+    public TMP_Text textField;       // Komponent TextMeshPro
+    [TextArea]
+    public string message;           // Tekst do wyświetlenia
+
+    public float delayBeforeText = 20f; // Opóźnienie w sekundach
+    public float letterDelay = 0.05f;   // Czas między literami
+
     private void Start()
     {
         // Ukryj przycisk/menu na początku
         mainMenu.SetActive(false);
+
+        // Ukryj panel tekstu
+        textPanel.SetActive(false);
 
         // Odtwórz film
         videoPlayer.Play();
 
         // Nasłuchiwanie zakończenia filmu
         videoPlayer.loopPointReached += OnVideoFinished;
+
+        // Uruchom wyświetlanie tekstu po 20 sekundach
+        StartCoroutine(ShowTextAfterDelay());
+    }
+
+    private IEnumerator ShowTextAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeText);
+
+        textPanel.SetActive(true);
+        textField.text = "";
+
+        foreach (char letter in message)
+        {
+            textField.text += letter;
+            yield return new WaitForSeconds(letterDelay);
+        }
     }
 
     private void OnVideoFinished(VideoPlayer vp)
@@ -30,3 +63,4 @@ public class Endscene : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 }
+
